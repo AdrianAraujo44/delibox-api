0518493ffc3c29a5a9e96c0ef0c6473a5d4bed50
AdrianAraujo <adrianaraujo247@gmail.com>
2022-08-03 23:04:08 -0300

    :recycle: refactor: alterando o código de status quando o usuário não esta autorizado

--- services/auth/index.js
+++ services/auth/index.js
@@ -12,7 +12,7 @@ const verifyToken = (req,res,next) => {
       next()
     })
   }else {
-    res.status(401).json("You are not authenticated")
+    res.status(200).json("You are not authenticated")
   }
 }
 
