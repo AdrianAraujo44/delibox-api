70c7efb5a9d423781f96a4b54fa3ff061e23d99e
Adrian Araujo <adrianaraujo247@gmail.com>
2022-09-08 10:36:03 -0300

    :sparkles: feat: adicionando taxa no model de order

--- models/order.js
+++ models/order.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const orderSchema = new Schema ({
-  deliveryId: {type: Schema.Types.ObjectId, ref: 'Delivery'},
-  type: {type:String, required: true}, 
+const orderSchema = new Schema({
+  deliveryId: { type: Schema.Types.ObjectId, ref: 'Delivery' },
+  type: { type: String, required: true },
   code: { type: String, required: true },
   status: [
     {
-      name: {type: String,  required: true},
-      date: {type: Date, required: true}
+      name: { type: String, required: true },
+      date: { type: Date, required: true }
     }
   ],
+  tax: {type: Number, default: 0},
   new: { type: Boolean, default: true, default: true },
   date: { type: Date, required: true },
   notes: { type: String, default: "" },
@@ -21,19 +22,19 @@ const orderSchema = new Schema ({
   },
 
   client: {
-    name: { type: String, required: true},
-    phone: { type: String, required: true},
-    address: { 
-      street: { type: String,},
-      number: { type: String,},
-      complement: { type: String,},
-      cep: { type: String,},
-      neighborhood: { type: String,},
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: {
+      street: { type: String, },
+      number: { type: String, },
+      complement: { type: String, },
+      cep: { type: String, },
+      neighborhood: { type: String, },
     }
   },
 
   products: [{
-    amount: { type: Number, required: true},
+    amount: { type: Number, required: true },
     item: {
       type: Schema.Types.ObjectId,
       ref: 'Product',
